33c3cb2e3600ee43afc74085e3ab9e3af5d5c5b5
Tron <tron@tron-X401A1.(none)>
2013-02-26 02:42:46 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpringLab::Application.config.secret_token = '8d5ded20bb7b6289abb6d09bd21fcc4f1111b8ee0445d32ca8418724611192ca5f6bb7d790cc98b0e63df34f6d394644922a047332fcaf8fa2a4c15a7376f420'
