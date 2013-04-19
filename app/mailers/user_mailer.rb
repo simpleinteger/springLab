f680781d6d79ab28123393e6a1b47737a44ca623
Tron <tron@tron-X401A1.(none)>
2013-04-19 07:47:19 -0700

    email .zip attachment and update email send content

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,12 +4,14 @@ class UserMailer < ActionMailer::Base
 
   def signup_confirmation(user)
     @user = user
+    mail.attachments["SpringLAB_Onboarding_Package.zip"] = File.read("#{Rails.root}/app/assets/zip/SpringLAB_Onboarding_Package.zip")
     mail to: user.email, subject: "Sign Up Confirmation"
   end
 
   def new_user_notification(user)
     @user = user
-    mail to: "patrickwu912@gmail.com", subject: "SpringLAB New User Notification"
+#    mail to: "patrickwu912@gmail.com", subject: "SpringLAB New User Notification"
+    mail to: "stevenr2d2@gmail.com", subject: "SpringLAB New User Notification"
   end
 end
 
